01e53eaddd9d65f2f8fb2f2882b59d3e7917e80e
Msvenkat1990 <msvenkat1990@gmail.com>
2023-12-07 22:23:05 +0530

    Login-User-Point

--- src/controllers/admin/userListController.js
+++ src/controllers/admin/userListController.js
@@ -7,25 +7,11 @@ const responseCode = require("../../utility/responseCode");
 // Login User
 const loginUser = async (req, res) => {
   try {
-    //bcrypt the password
+    // bcrypt the password
     let username = req.body.userName;
     let password = req.body.passWord;
-    const hashedPassword = await bcrypt.hash(req.body.passWord, 10);
     const secretKey = "234b kqw%$##%UGjV&^*&";
 
-    // Example payload (information you want to include in the token)
-    const payload = {
-      username: req.body.userName,
-    };
-
-    // Generate a JWT
-    const token = jwt.sign(payload, secretKey, { expiresIn: "1h" });
-
-    console.log("Generated JWT:", token);
-    // let data = new userSchema({
-    //   userName: req.body.userName,
-    //   passWord: hashedPassword,
-    // });
     const users = await userSchema.aggregate([
       {
         $match: {
@@ -46,22 +32,27 @@ const loginUser = async (req, res) => {
       const passwordMatch = await bcrypt.compare(password, user.passWord);
     
       if (passwordMatch) {
+        const payload = {
+          username: req.body.userName,
+        };
+
+        // Generate a JWT
+        const token = jwt.sign(payload, secretKey, { expiresIn: "1h" });
+
         console.log('User authenticated');
-        res.status(200).json("User authenticated" );
+        res.status(200).json({ message: 'User authenticated', token });
       } else {
-        res.json("Invalid password" );
         console.log('Invalid password');
-      }406
+        res.status(401).json({ error: 'Invalid password' });
+      }
     } else {
-      res.json("Invalid password" );
       console.log('User not found');
+      res.status(404).json({ error: 'User not found' });
     }
-    console.log('login--->',login);
-    // let saveData = await data.save();
-    // res.json(saveData);
   } catch (error) {
-    res.json({ error: "Error creating user" });
+    res.status(500).json({ error: "Error creating user" });
   }
 };
+
 
 module.exports = { loginUser };
